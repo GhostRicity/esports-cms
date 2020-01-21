4b58bb642b342a3eeaeefa14f8c66c7bf3a741ed
Eduards Birznieks <eduards.latvia@gmail.com>
2020-01-21 15:07:44 +0100

    sd

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/content/research`,
+        name: `research`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         path: `${__dirname}/content/assets`,
         name: `assets`,
       },
