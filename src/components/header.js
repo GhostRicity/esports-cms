787e0bf0f46c3b3984354859eab37b29856e9305
Eduards Birznieks <eduards.latvia@gmail.com>
2022-07-04 21:50:51 +0200

    Updated Header with dependencie

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React from "react"
 //bootstrap
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 
 const Header = () => (
